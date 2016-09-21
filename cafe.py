8a23c9f1be36764071daba543002052f52110474
Ture Pålsson <ture@lysator.liu.se>
2016-09-21 14:23:13 +0200

    Prettify code
    Fixes #1

--- cafe.py
+++ cafe.py
@@ -1,24 +1,30 @@
 import json, sys
 import psycopg2
 
-with psycopg2.connect(dbname = 'sweden') as conn:
-    c = conn.cursor()
-    c.execute('''SELECT id, ST_AsGeoJSON(g) AS g, tags->'name' AS name
-      FROM feature
-      WHERE tags @> 'amenity=>cafe'
-            AND st_dwithin(g, (SELECT g FROM feature
-                                WHERE tags->'name' LIKE 'Ejes%'),
-                           1.0/300)''')
-    features = []
-    for row in c.fetchall():
-        fid, geomstr, name = row
-        geom = json.loads(geomstr)
-        props = { 'id': fid, 'name': name }
-        obj = { 'type': 'Feature', 'properties': props, 'geometry': geom }
-        features.append(obj)
+def cafes(out):
+    with psycopg2.connect(dbname = 'sweden') as conn:
+        c = conn.cursor()
+        c.execute('''SELECT id, ST_AsGeoJSON(g) AS g, tags->'name' AS name
+          FROM feature
+          WHERE tags @> 'amenity=>cafe'
+                AND st_dwithin(g, (SELECT g FROM feature
+                                    WHERE tags->'name' LIKE 'Ejes%'),
+                               1.0/300)''')
+        features = []
+        for row in c.fetchall():
+            fid, geomstr, name = row
+            geom = json.loads(geomstr)
+            props = { 'id': fid, 'name': name }
+            obj = { 'type': 'Feature', 'properties': props, 'geometry': geom }
+            features.append(obj)
+    
+    coll = { 'type': 'FeatureCollection', 'features': features }
+    
+    json.dump(coll, out)
+    out.write('\n')
 
-coll = { 'type': 'FeatureCollection', 'features': features }
+def main():
+    cafes(sys.stdout)
 
-json.dump(coll, sys.stdout)
-sys.stdout.write('\n')
-
+if __name__ == '__main__':
+    main()
